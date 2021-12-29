cb24644de12ffbefe4a5813e55f8ba4a0fb34197
zeinab-elnazeir <75196562+zeinab-elnazeir@users.noreply.github.com>
2021-12-29 06:19:15 +0200

    add mobile grid module

---
+++ ecommerce_mobile_grid/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ecommerce mobile grid",
+
+
+    'description': 'Modify grid view in shop to appear in mobile 2 X 3.',
+
+    'author': "Zeinab Elnazeir",
+    'website': "",
+   
+    'category': '',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'website_sale'
+    ],
+
+    # always loaded
+    'data': [
+        'views/assets.xml',
+
+    ],
+
+}
